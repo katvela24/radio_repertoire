ee04916fc375cd2255b7914f9cd0d75fbab5b101
katvela24 <156866399+katvela24@users.noreply.github.com>
2024-03-01 19:18:45 -0500

    linked leaflet API and scripts along with initial code

---
+++ script.js
@@ -0,0 +1,14 @@
+// 
+if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function(position){
+        var map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    var marker = L.marker([51.5, -0.09]).addTo(map);
+    })
+    
+
+}
+else{alert("location not shared")}
